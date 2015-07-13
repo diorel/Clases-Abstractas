2fbb6d21a40f343a3d136ad4c8492f47c3f6267a
Diorelyon Amador <diorel_x@hotmail.com>
2015-07-12 23:55:52 -0500

    subir
    subir1

---
+++ ProgramaClaseAbtractaC/ProgramaClaseAbtractaC/clsAbsClientes.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgramaClaseAbtractaC
+{
+    public abstract class clsAbsClientes
+    {
+        public abstract int Id { get; set; }
+        public abstract string Nombre { get; set; }
+        public abstract string Clave { get; set; }
+        public abstract string RFC { get; set; }
+        public int string TipoRegimen { get; set; }
+        public abstract string NombreContact { get; set; }
+
+       
+
+    }
+}
---
+++ ProgramaClaseAbtractaC/ProgramaClaseAbtractaC/clsBaceClientes.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgramaClaseAbtractaC
+{
+    public class clsBaceClientes : clsAbsClientes
+    {
+        public override int Id { get; set; }
+        public override string Nombre { get; set;}
+        public override string Clave { get; set; }
+        public override string RFC { get; set; }
+        public override int Tiporegimen { get; set; }
+        public override string NombreContact { get; set; }
+
+    }
+}
